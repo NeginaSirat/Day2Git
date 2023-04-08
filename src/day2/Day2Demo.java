5edc570d3c4650c2c8136128682f9a2d79f4ed23
hedas <hedas@10.0.0.133>
2023-04-07 18:06:05 -0700

    implemented printInfo()

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -4,6 +4,11 @@ public class Day2Demo {
 	public static void main(String[] args) {
 		System.out.println("day 2");
 	}
+	
+	public void PrintInfo(String name, String city) {
+		System.out.println("Name:"+ name);
+		System.out.println("City:"+ city);
+	}
 
 	public void getInfo() {
 		System.out.println("Pushing code...");
